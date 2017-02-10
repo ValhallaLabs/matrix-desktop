d6a0988340aef5faaa8fdd7c8d5f5c2cdb7e213a
Psihey <psihey1@gmail.com>
2017-02-10 11:32:05 +0200

    create complete login window with css and crossing from login to main layout

--- src/main/java/ua/softgroup/matrix/desktop/controllerjavafx/MainController.java
+++ src/main/java/ua/softgroup/matrix/desktop/controllerjavafx/MainController.java
@@ -1,7 +1,54 @@
 package ua.softgroup.matrix.desktop.controllerjavafx;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 /**
  * Created by AndriiBei on 09.02.2017.
  */
 public class MainController {
+    private ClassLoader classLoader;
+    private FXMLLoader loader;
+    private BorderPane mainLayout;
+    private Stage primaryStage;
+
+    public void startMainControllerLayout() {
+        primaryStage=new Stage();
+        startMainLayout();
+        startProjectLayout();
+    }
+
+    private void startMainLayout() {
+        try {
+            classLoader = getClass().getClassLoader();
+            loader = new FXMLLoader();
+            loader.setLocation(classLoader.getResource("fxml/mainLayout.fxml"));
+             mainLayout = loader.load();
+            Scene scene = new Scene(mainLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            primaryStage.setMinWidth(1200);
+            primaryStage.setMinHeight(800);
+            primaryStage.setResizable(false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startProjectLayout() {
+        try {
+            classLoader = getClass().getClassLoader();
+            loader = new FXMLLoader();
+            loader.setLocation(classLoader.getResource("fxml/projectsLayout.fxml"));
+          AnchorPane projectsLayout = loader.load();
+            mainLayout.setCenter(projectsLayout);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/ua/softgroup/matrix/desktop/start/Main.java
+++ src/main/java/ua/softgroup/matrix/desktop/start/Main.java
@@ -3,21 +3,16 @@ package ua.softgroup.matrix.desktop.start;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 import java.io.IOException;
 
 
 public class Main extends Application {
 
     private Stage loginStage;
-    private ClassLoader classLoader;
-    private FXMLLoader loader;
-    private BorderPane mainLayout;
-    private AnchorPane projectsLayout;
-    private Pane loginLayout;
 
     public static void main(String[] args) {
         launch(args);
@@ -27,50 +22,20 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         this.loginStage = primaryStage;
         this.loginStage.setTitle("SuperVisor");
-        setLogin();
-//        setLoginPane();
-//        setProjectPane();
+        startLoginLayout();
     }
 
-    private void setLogin() {
-
+    private void startLoginLayout() {
         try {
-            classLoader = getClass().getClassLoader();
-            loader = new FXMLLoader();
+            ClassLoader classLoader = getClass().getClassLoader();
+            FXMLLoader loader = new FXMLLoader();
             loader.setLocation(classLoader.getResource("fxml/loginLayout.fxml"));
-            loginLayout=loader.load();
+            Pane loginLayout = loader.load();
             Scene scene = new Scene(loginLayout);
             loginStage.setScene(scene);
+            loginStage.initStyle(StageStyle.UTILITY);
+            loginStage.setResizable(false);
             loginStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private void setLoginPane() {
-        try {
-            classLoader = getClass().getClassLoader();
-            loader = new FXMLLoader();
-            loader.setLocation(classLoader.getResource("fxml/mainLayout.fxml"));
-            mainLayout = loader.load();
-            Scene scene = new Scene(mainLayout);
-            loginStage.setScene(scene);
-            loginStage.show();
-            loginStage.setMinWidth(1200);
-            loginStage.setMinHeight(800);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void setProjectPane() {
-        try {
-            classLoader = getClass().getClassLoader();
-            loader = new FXMLLoader();
-            loader.setLocation(classLoader.getResource("fxml/projectsLayout.fxml"));
-            projectsLayout = loader.load();
-            mainLayout.setCenter(projectsLayout);
         } catch (IOException e) {
             e.printStackTrace();
         }
