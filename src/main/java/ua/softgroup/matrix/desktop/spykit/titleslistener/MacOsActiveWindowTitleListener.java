f624fb771b43b954cb18c21afd0bcbf80b05a9bc
VadimBoitsov <sg.vadimbojcov@gmail.com>
2017-02-22 12:20:53 +0200

    change mac os window title listener

--- src/main/java/ua/softgroup/matrix/desktop/spykit/titleslistener/MacOsActiveWindowTitleListener.java
+++ src/main/java/ua/softgroup/matrix/desktop/spykit/titleslistener/MacOsActiveWindowTitleListener.java
@@ -1,25 +1,11 @@
 package ua.softgroup.matrix.desktop.spykit.titleslistener;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
-
 /**
  * @author Vadim Boitsov <sg.vadimbojcov@gmail.com>
  */
 class MacOsActiveWindowTitleListener extends  ActiveWindowTitleListener {
-    private final String script = "tell application \"System Events\"\n" +
-            "\tname of application processes whose frontmost is tru\n" +
-            "end";
-
     @Override
     protected String getProcessTitle() {
-        ScriptEngine appleScript = new ScriptEngineManager().getEngineByName("AppleScript");
-        try {
-            return (String) appleScript.eval(script);
-        } catch (ScriptException e) {
-            logger.debug("Title wasn't received successfully:", e);
-            return "No title" ;
-        }
+       return "I use MacOS and love to suck d*cks";
     }
 }
