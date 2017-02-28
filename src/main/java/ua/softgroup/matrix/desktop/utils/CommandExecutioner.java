6fa9ce921b08a63b0550c025693fd9553ff1ea48
VadimBoitsov <sg.vadimbojcov@gmail.com>
2017-02-28 13:21:57 +0200

    start adding command executioner

---
+++ src/main/java/ua/softgroup/matrix/desktop/utils/CommandExecutioner.java
@@ -0,0 +1,18 @@
+package ua.softgroup.matrix.desktop.utils;
+
+import ua.softgroup.matrix.server.desktop.api.ServerCommands;
+
+import java.net.Socket;
+
+/**
+ * @author Vadim Boitsov <sg.vadimbojcov@gmail.com>
+ */
+public class CommandExecutioner {
+
+
+    public static void sendCommand(Socket socket, ServerCommands serverCommand) {
+
+    }
+
+
+}
