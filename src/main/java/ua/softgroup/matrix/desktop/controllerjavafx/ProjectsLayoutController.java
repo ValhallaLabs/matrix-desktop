49d58d886ced67c219f94bf9db5ed0df225ceac3
Psihey <psihey1@gmail.com>
2017-03-03 16:00:22 +0200

    Change some Layout for better view

--- src/main/java/ua/softgroup/matrix/desktop/controllerjavafx/ProjectsLayoutController.java
+++ src/main/java/ua/softgroup/matrix/desktop/controllerjavafx/ProjectsLayoutController.java
@@ -7,8 +7,6 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
-
-
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.*;
