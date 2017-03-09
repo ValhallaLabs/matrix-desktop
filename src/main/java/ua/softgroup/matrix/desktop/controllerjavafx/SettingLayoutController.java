2ba64ceec811acc74dd2ba3de3a4e04a55dc1c68
Psihey <psihey1@gmail.com>
2017-03-09 14:45:47 +0200

    Connect setting window with contoller and ConfinManager

--- src/main/java/ua/softgroup/matrix/desktop/controllerjavafx/SettingLayoutController.java
+++ src/main/java/ua/softgroup/matrix/desktop/controllerjavafx/SettingLayoutController.java
@@ -1,34 +1,44 @@
 package ua.softgroup.matrix.desktop.controllerjavafx;
 
 import javafx.event.ActionEvent;
-import org.apache.commons.configuration.CompositeConfiguration;
-import org.apache.commons.configuration.PropertiesConfiguration;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ua.softgroup.matrix.desktop.utils.ConfigManager;
-import ua.softgroup.matrix.desktop.utils.SocketProvider;
-
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import static java.nio.file.StandardOpenOption.CREATE;
-import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 /**
  * @author Andrii Bei <sg.andriy2@gmail.com>
  */
 public class SettingLayoutController {
     public static final Logger logger = LoggerFactory.getLogger(SettingLayoutController.class);
+    @FXML
+    public TextField labelHost;
+    @FXML
+    public TextField labelPort;
 
-    /**
-     * TODO:
-     * 1) Provide method for set a host and port to view. To get values use methods {@link ConfigManager#getHost()}
-     * and {@link ConfigManager#getPort()}.
-     * 2) Provide listener for set configs to default . Use method {@link ConfigManager#setConfigToDefault()}
-     * 3) Provide listener for saving new configs. Use method {@link ConfigManager#saveNewConfig(String, String)}
-     */
+    @FXML
+    public void initialize() {
+        getPortAndHostFromConfigManager();
+    }
+
+    private void getPortAndHostFromConfigManager() {
+        labelHost.setText(ConfigManager.getHost());
+        labelPort.setText(ConfigManager.getPort());
+    }
+
+    public void saveSettings(ActionEvent actionEvent) {
+        ConfigManager.saveNewConfig(labelHost.getText(), labelPort.getText());
+        labelHost.getScene().getWindow().hide();
+    }
+
+    public void cancelSettings(ActionEvent actionEvent) {
+        labelHost.getScene().getWindow().hide();
+    }
+
+    public void resetToDefaultSettings(ActionEvent actionEvent) {
+        ConfigManager.setConfigToDefault();
+        getPortAndHostFromConfigManager();
+    }
 }
