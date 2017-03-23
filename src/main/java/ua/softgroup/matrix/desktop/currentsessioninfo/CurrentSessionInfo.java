a5091ff4b297bb69845ffe269503a5b66d852d21
VadimBoitsov <sg.vadimbojcov@gmail.com>
2017-03-23 11:33:49 +0200

    clean current session info class

--- src/main/java/ua/softgroup/matrix/desktop/currentsessioninfo/CurrentSessionInfo.java
+++ src/main/java/ua/softgroup/matrix/desktop/currentsessioninfo/CurrentSessionInfo.java
@@ -10,15 +10,10 @@ import java.util.Set;
 /**
  * Created by Vadim on 10.02.2017.
  */
-// TODO package structure
 public class CurrentSessionInfo {
     private static InitializeModel initializeModel = new InitializeModel();
     private static SynchronizationModel synchronizationModel = null;
     private static long projectId;
-
-//    public static InitializeModel getInitializeModel() {
-//        return initializeModel;
-//    }
 
     public static void setInitializeModel(InitializeModel initializeModel) {
         CurrentSessionInfo.initializeModel = initializeModel;
@@ -28,41 +23,21 @@ public class CurrentSessionInfo {
         return initializeModel.getToken();
     }
 
-//    public void setToken(String token) {
-//        initializeModel.setToken(token);
-//    }
-
     public static Set<ProjectModel> getProjectModels() {
         return initializeModel.getProjectModels();
     }
-
-//    public void setProjectModels(Set<ProjectModel> projectModels) {
-//        initializeModel.setProjectModels(projectModels);
-//    }
 
     public static int getIdlePeriod() {
         return initializeModel.getIdlePeriod();
     }
 
-//    public void setIdlePeriod(long idlePeriod) {
-//        initializeModel.setIdlePeriod(idlePeriod);
-//    }
-
     public static int getScreenshotFrequency() {
         return initializeModel.getScreenshotFrequency();
     }
 
-//    public void setScreenshotFrequency(long screenshotFrequency) {
-//        initializeModel.setScreenshotFrequency(screenshotFrequency);
-//    }
-
     public static int getCheckPointPeriod() {
         return initializeModel.getCheckPointPeriod();
     }
-
-//    public void setCheckPointFrequency(long checkPointFrequency) {
-//        initializeModel.setCheckPointFrequency(checkPointFrequency);
-//    }
 
     public static SynchronizationModel getSynchronizationModel() {
         return synchronizationModel;
