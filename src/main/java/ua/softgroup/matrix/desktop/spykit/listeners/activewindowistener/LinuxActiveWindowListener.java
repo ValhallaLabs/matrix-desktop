1f2ffa1681dcf75f84311198e1240f54a308c873
vboitsov <vadimboitsov1@gmail.com>
2017-03-24 16:59:01 +0200

    provide xdotool instead of JNA for linux

--- src/main/java/ua/softgroup/matrix/desktop/spykit/listeners/activewindowistener/LinuxActiveWindowListener.java
+++ src/main/java/ua/softgroup/matrix/desktop/spykit/listeners/activewindowistener/LinuxActiveWindowListener.java
@@ -1,5 +1,9 @@
 package ua.softgroup.matrix.desktop.spykit.listeners.activewindowistener;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * @author Vadim Boitsov <sg.vadimbojcov@gmail.com>
  */
@@ -7,7 +11,16 @@ class LinuxActiveWindowListener extends ActiveWindowListener {
 
     @Override
     protected String getProcessTitle() {
-        return "hey!";
+        try {
+            Process p = Runtime.getRuntime().exec("xdotool getwindowfocus getwindowname");
+            BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String title = br.readLine();
+            p.waitFor();
+            p.destroy();
+            return title;
+        } catch (InterruptedException | IOException e) {
+            return "Unable to get window, something went wrong";
+        }
     }
 
 }
