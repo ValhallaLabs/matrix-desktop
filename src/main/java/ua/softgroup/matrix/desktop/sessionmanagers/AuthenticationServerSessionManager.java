a01be1f8e4dfe8731c47958fc8da08b51fd1ec89
VadimBoitsov <sg.vadimbojcov@gmail.com>
2017-03-27 10:50:38 +0300

    add isConnection opened method to ayth manager

--- src/main/java/ua/softgroup/matrix/desktop/sessionmanagers/AuthenticationServerSessionManager.java
+++ src/main/java/ua/softgroup/matrix/desktop/sessionmanagers/AuthenticationServerSessionManager.java
@@ -174,4 +174,8 @@ public class AuthenticationServerSessionManager {
             socketDisposable.dispose();
         }
     }
+
+    public boolean isConnectionOpened() {
+        return !socketDisposable.isDisposed();
+    }
 }
